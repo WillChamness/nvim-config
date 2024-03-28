b915caea7f987239b47ae3fa2ddc0579483d7d75
WillChamness <wzchamness@gmail.com>
2024-03-28 01:51:10 -0400

    update toggleterm plugin config
    change keymap to prevent escape keypress from exiting out of the
    terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,7 +7,6 @@ return {
 		-- navigate around the windows
 		function _G.SET_TERMINAL_KEYMAPS()
 			local opts = { noremap = true }
-			vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
 			vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
 			vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
 			vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
